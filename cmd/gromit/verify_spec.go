168071439016308bfd8fbc78e2139590601c39a2
Dan Abrams <dabrams@example.com>
2026-02-18 19:57:13 +0000

    Add create-beads flag to verify-spec

--- cmd/gromit/verify_spec.go
+++ cmd/gromit/verify_spec.go
@@ -4,6 +4,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var verifySpecCreateBeads bool
+
 var verifySpecCmd = &cobra.Command{
 	Use:   "verify-spec <spec>",
 	Short: "Verify a spec's acceptance criteria",
@@ -14,5 +16,6 @@ var verifySpecCmd = &cobra.Command{
 }
 
 func init() {
+	verifySpecCmd.Flags().BoolVar(&verifySpecCreateBeads, "create-beads", false, "Create fix beads for failing criteria")
 	rootCmd.AddCommand(verifySpecCmd)
 }
--- cmd/gromit/verify_spec_test.go
+++ cmd/gromit/verify_spec_test.go
@@ -15,3 +15,14 @@ func TestVerifySpecCmd_Registration(t *testing.T) {
 		t.Error("verify-spec command not registered with rootCmd")
 	}
 }
+
+func TestVerifySpecCmd_Flags(t *testing.T) {
+	flag := verifySpecCmd.Flags().Lookup("create-beads")
+	if flag == nil {
+		t.Error("verify-spec should have --create-beads flag")
+		return
+	}
+	if flag.Value.Type() != "bool" {
+		t.Errorf("--create-beads flag should be bool, got %s", flag.Value.Type())
+	}
+}
