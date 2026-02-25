62c313e044e154ea2c8bb3447b46a8f7f9a592bf
Dan Abrams <dabrams@example.com>
2026-02-25 12:52:13 +0000

    green: implement loader stub

---
+++ internal/experiment/loader.go
@@ -0,0 +1,6 @@
+package experiment
+
+// LoadExperiments returns the experiments defined in the given directory.
+func LoadExperiments(dir string) ([]*Experiment, error) {
+    return []*Experiment{}, nil
+}
