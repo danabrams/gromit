7db36ddca5f7ad83c4060f955a8f647098ed0f7c
Dan Abrams <dabrams@example.com>
2026-02-25 12:53:09 +0000

    green: implement experiment loader

--- internal/experiment/loader.go
+++ internal/experiment/loader.go
@@ -1,6 +1,34 @@
 package experiment
 
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
 // LoadExperiments returns the experiments defined in the given directory.
 func LoadExperiments(dir string) ([]*Experiment, error) {
-    return []*Experiment{}, nil
+	pattern := filepath.Join(dir, "*.yaml")
+	files, err := filepath.Glob(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	var exps []*Experiment
+	for _, path := range files {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+
+		var exp Experiment
+		if err := yaml.Unmarshal(data, &exp); err != nil {
+			return nil, err
+		}
+
+		exps = append(exps, &exp)
+	}
+
+	return exps, nil
 }
