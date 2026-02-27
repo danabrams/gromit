4e5d0b6a9874f04469f26bf91db269a0beefb970
Dan Abrams <dabrams@example.com>
2026-02-27 16:55:10 +0000

    red: test for pipeline.Deps adapter wiring completeness

---
+++ cmd/gromit/adapter_pipeline_deps_wiring_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/danabrams/gromit/internal/pipeline"
+)
+
+// TestPipelineDepsWiring_AllAdaptersImplementInterfaces verifies that all required
+// pipeline.Deps interfaces have corresponding adapter implementations available for wiring.
+func TestPipelineDepsWiring_AllAdaptersImplementInterfaces(t *testing.T) {
+	t.Parallel()
+
+	// Verify each pipeline.Deps field can be satisfied by an adapter type.
+	// This confirms all adapters exist and implement their required interfaces.
+	var _ pipeline.LLMClient = (*claudeClientAdapter)(nil)
+	var _ pipeline.ReviewInvoker = (*claudeClientAdapter)(nil)
+	var _ pipeline.TrackerClient = (*trackerClientAdapter)(nil)
+	var _ pipeline.BacklogClient = (*cliBacklogClient)(nil)
+	var _ pipeline.BacklogWriter = (*cliBacklogClient)(nil)
+	var _ pipeline.RefineRenderer = (*refinePromptRenderer)(nil)
+	var _ pipeline.PlanRenderer = (*planPromptRenderer)(nil)
+	var _ pipeline.DecomposeRenderer = (*decomposePromptRenderer)(nil)
+	var _ pipeline.ReviewRenderer = (*cliPromptRenderer)(nil)
+	var _ pipeline.ExploreRenderer = (*explorePromptRenderer)(nil)
+	var _ pipeline.LearningsManager = (*cliLearningsManager)(nil)
+	var _ pipeline.StateManager = (*cliStateManager)(nil)
+	var _ pipeline.LogWriter = (*cliLogWriter)(nil)
+
+	t.Log("All pipeline.Deps interfaces can be satisfied by adapters")
+}
