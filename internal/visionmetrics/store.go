de238b934584593dc2f210a9862742ed7889dd13
Dan Abrams <dabrams@example.com>
2026-02-28 04:31:13 +0000

    green: implement AppendRecord function for JSONL file writing

--- internal/visionmetrics/store.go
+++ internal/visionmetrics/store.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+const fileMode = 0644
+
 // LoadRecords reads a JSONL file and returns the list of Records.
 func LoadRecords(path string) ([]Record, error) {
 	file, err := os.Open(path)
@@ -33,4 +35,32 @@ func LoadRecords(path string) ([]Record, error) {
 	}
 
 	return records, nil
+}
+
+// AppendRecord appends a Record to a JSONL file.
+func AppendRecord(path string, record Record) error {
+	// Open file for appending, creating if it doesn't exist
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, fileMode)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Marshal record to JSON and write as a line
+	data, err := json.Marshal(record)
+	if err != nil {
+		return err
+	}
+
+	writer := bufio.NewWriter(file)
+	_, err = writer.Write(data)
+	if err != nil {
+		return err
+	}
+	_, err = writer.WriteString("\n")
+	if err != nil {
+		return err
+	}
+
+	return writer.Flush()
 }
