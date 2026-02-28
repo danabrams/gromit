ed021bdc92ff99d948774b8408e20295ea9be380
Dan Abrams <dabrams@example.com>
2026-02-28 03:35:26 +0000

    red: test for BranchRouter interface

---
+++ internal/runner/specbranch_interfaces_test.go
@@ -0,0 +1,15 @@
+package runner
+
+import (
+	"testing"
+
+	"github.com/danabrams/gromit/internal/runner/specbranch"
+)
+
+// TestBranchRouterInterface verifies that Router implements BranchRouter interface.
+// This test will fail compilation if BranchRouter doesn't exist or Router doesn't implement it.
+func TestBranchRouterInterface(t *testing.T) {
+	t.Parallel()
+	var _ BranchRouter = (*specbranch.Router)(nil)
+	t.Log("Router implements BranchRouter")
+}
