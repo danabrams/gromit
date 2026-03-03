8ccaf0a164bfd398d24e5ddb6991a3a382a3c2c5
Dan Abrams <me@thedanabrams.com>
2026-03-02 20:17:48 -0500

    red: test WaitForProcessCapacity respects cancel during non-pressured path

--- internal/procutil/process_capacity_context_test.go
+++ internal/procutil/process_capacity_context_test.go
@@ -3,6 +3,7 @@ package procutil
 import (
 	"context"
 	"errors"
+	"sync"
 	"testing"
 	"time"
 )
@@ -18,6 +19,37 @@ func TestWaitForProcessCapacityCanceledBeforePressuredCheck(t *testing.T) {
 	cancel()
 
 	err := WaitForProcessCapacity(ctx, 50*time.Millisecond)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatalf("WaitForProcessCapacity() error = %v, want context.Canceled", err)
+	}
+}
+
+func TestWaitForProcessCapacityDetectsContextCancelWhileNonPressured(t *testing.T) {
+	original := processCreationPressuredFn
+	t.Cleanup(func() {
+		processCreationPressuredFn = original
+	})
+
+	called := make(chan struct{})
+	release := make(chan struct{})
+	var once sync.Once
+	processCreationPressuredFn = func() (bool, error) {
+		once.Do(func() { close(called) })
+		<-release
+		return false, nil
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- WaitForProcessCapacity(ctx, 50*time.Millisecond)
+	}()
+
+	<-called
+	cancel()
+	close(release)
+
+	err := <-errCh
 	if !errors.Is(err, context.Canceled) {
 		t.Fatalf("WaitForProcessCapacity() error = %v, want context.Canceled", err)
 	}
