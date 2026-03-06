be670fa12dc77d9924f06fcc9322d3820f23c59f
Dan Abrams <me@thedanabrams.com>
2026-03-06 01:12:09 -0500

    red: test for fake interface compile checks

---
+++ internal/v2/testutil/fakes_test.go
@@ -0,0 +1,15 @@
+package testutil
+
+import (
+    "github.com/danabrams/gromit/internal/v2/adapter"
+    "github.com/danabrams/gromit/internal/v2/adapter/llm"
+    "github.com/danabrams/gromit/internal/v2/adapter/tasktracker"
+)
+
+var (
+    _ llm.LLMProvider           = (*FakeLLM)(nil)
+    _ tasktracker.TaskTracker    = (*FakeTaskTracker)(nil)
+    _ adapter.TaskTrackerAdapter = (*FakeTaskTracker)(nil)
+    _ adapter.PresenterAdapter    = (*FakePresenter)(nil)
+    _ adapter.GitAdapter          = (*FakeGit)(nil)
+)
