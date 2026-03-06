574a0c8764796ff96b0bbe85c14344f860b064f7
Dan Abrams <me@thedanabrams.com>
2026-03-06 05:58:56 -0500

    red: test for tasktracker contract

---
+++ internal/v2/adapter/tasktracker/interface_contract_test.go
@@ -0,0 +1,15 @@
+package tasktracker
+
+import (
+    "context"
+    "testing"
+)
+
+func TestTaskTrackerContract(t *testing.T) {
+    var _ interface {
+        NextBead(context.Context, TaskTrackerNextBeadRequest) (*TaskTrackerNextBeadResponse, error)
+        CreateBead(context.Context, TaskTrackerCreateBeadRequest) (*TaskTrackerCreateBeadResponse, error)
+        CloseBead(context.Context, TaskTrackerCloseBeadRequest) (*TaskTrackerCloseBeadResponse, error)
+        QueryBeads(context.Context, TaskTrackerQueryBeadsRequest) (*TaskTrackerQueryBeadsResponse, error)
+    } = (TaskTracker)(nil)
+}
