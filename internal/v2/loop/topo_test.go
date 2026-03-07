aafc6d354ee157407bd8a09ac13fa3a3541c15a0
Dan Abrams <me@thedanabrams.com>
2026-03-07 15:48:07 -0500

    red: test TopologicalSort puts dependency before dependent

---
+++ internal/v2/loop/topo_test.go
@@ -0,0 +1,27 @@
+package loop
+
+import (
+	"testing"
+
+	"github.com/danabrams/gromit/internal/bead"
+)
+
+func TestTopologicalSort_DependencyBeforeDependent(t *testing.T) {
+	t.Parallel()
+
+	beads := []*bead.Bead{
+		{ID: "child", DependsOn: []bead.Dependency{{ID: "root"}}},
+		{ID: "root"},
+	}
+
+	got, err := TopologicalSort(beads)
+	if err != nil {
+		t.Fatalf("TopologicalSort: %v", err)
+	}
+	if len(got) != 2 {
+		t.Fatalf("got %d beads, want 2", len(got))
+	}
+	if got[0].ID != "root" || got[1].ID != "child" {
+		t.Fatalf("order = [%s %s], want [root child]", got[0].ID, got[1].ID)
+	}
+}
