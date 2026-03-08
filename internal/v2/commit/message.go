8fbfd8bf7bf729704c9763a16b94b1f3ec78ac16
Dan Abrams <me@thedanabrams.com>
2026-03-08 03:11:39 -0400

    green: implement structured commit message parser

--- internal/v2/commit/message.go
+++ internal/v2/commit/message.go
@@ -1,8 +1,42 @@
 package commit
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+// Message holds parsed structured commit message fields.
+type Message struct {
+	BeadID    string
+	StageName string
+	Iteration int
+	Decision  string
+}
 
 // FormatMessage encodes a structured commit message.
 func FormatMessage(beadID, stageName string, iteration int, decision string) string {
 	return fmt.Sprintf("[bead:%s/%s/iter:%d] %s", beadID, stageName, iteration, decision)
+}
+
+var messageRe = regexp.MustCompile(`^\[bead:([^/]+)/([^/]+)/iter:(\d+)\]\s+(.+)$`)
+
+// ParseMessage decodes a structured commit message into its components.
+func ParseMessage(msg string) (Message, error) {
+	matches := messageRe.FindStringSubmatch(msg)
+	if matches == nil {
+		return Message{}, fmt.Errorf("invalid commit message format")
+	}
+
+	iteration, err := strconv.Atoi(matches[3])
+	if err != nil {
+		return Message{}, fmt.Errorf("invalid iteration: %w", err)
+	}
+
+	return Message{
+		BeadID:    matches[1],
+		StageName: matches[2],
+		Iteration: iteration,
+		Decision:  matches[4],
+	}, nil
 }
