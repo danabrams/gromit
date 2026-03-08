d76422d590b674098938fd1f97186275d9133cce
Dan Abrams <me@thedanabrams.com>
2026-03-08 07:32:42 -0400

    green: implement LearnContext, LearningResult, and ExtractLearning

--- internal/v2/debug/errors.go
+++ internal/v2/debug/errors.go
@@ -3,5 +3,6 @@ package debug
 import "errors"
 
 var (
-	ErrNilFixContext = errors.New("fix context is nil")
+	ErrNilFixContext   = errors.New("fix context is nil")
+	ErrNilLearnContext = errors.New("learn context is nil")
 )
