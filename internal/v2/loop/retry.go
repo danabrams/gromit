3eea084b241e5803bb880572cc9adec2b011fb3b
Dan Abrams <me@thedanabrams.com>
2026-03-08 10:29:38 -0400

    green: initialize retry iteration counter map

--- internal/v2/loop/retry.go
+++ internal/v2/loop/retry.go
@@ -6,6 +6,9 @@ type stageIterationKey struct {
 }
 
 func (b *BeadLoop) nextStageIteration(beadID, stageName string) int {
+	if b.stageIterations == nil {
+		b.stageIterations = make(map[stageIterationKey]int)
+	}
 	key := stageIterationKey{beadID: beadID, stageName: stageName}
 	b.stageIterations[key]++
 	return b.stageIterations[key]
