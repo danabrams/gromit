3444ade53ded23ef5c588bc8125238dd56917d59
Dan Abrams <me@thedanabrams.com>
2026-03-28 19:50:38 -0400

    red: test for debug help mentions jobs

--- cmd/gromit/debug_help_golden_test.go
+++ cmd/gromit/debug_help_golden_test.go
@@ -1,8 +1,21 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestDebugHelpMatchesGolden(t *testing.T) {
 	t.Parallel()
 	assertHelpMatchesGolden(t, "cmd/gromit/testdata/golden/debug.help.txt", debugCmd.Long)
+}
+
+func TestDebugHelpMentionsDebugJobs(t *testing.T) {
+	t.Parallel()
+	text := strings.ToLower(debugCmd.Long)
+	for _, job := range []string{"diagnose", "fix", "learn"} {
+		if !strings.Contains(text, job) {
+			t.Fatalf("debug help missing mention of %s job", job)
+		}
+	}
 }
