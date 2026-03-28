46c83ba5c8471e92ba2a50d3fd4eaa1e779515bc
Dan Abrams <me@thedanabrams.com>
2026-03-28 19:50:15 -0400

    green: register debug command alongside other root commands

--- cmd/gromit/main.go
+++ cmd/gromit/main.go
@@ -171,6 +171,7 @@ func init() {
 func registerRootCommands(root *cobra.Command) {
 	root.AddCommand(runCmd)
 	root.AddCommand(run2Cmd)
+	root.AddCommand(debugCmd)
 	root.AddCommand(statusCmd)
 	root.AddCommand(readyCmd)
 	root.AddCommand(retroCmd)
